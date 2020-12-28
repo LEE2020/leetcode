df776274c6b5504510c976f5c5ef726489d06ee6
LEE2020 <frona_li@126.com>
2020-12-28 21:02:28 +0800

    add

---
+++ coding_reversion/07_rebuildbinarytree.py
@@ -0,0 +1,52 @@
+'''
+输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+
+ 
+
+例如，给出
+
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+返回如下的二叉树：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+        #创建当前节点
+        node = TreeNode(preorder[0]) 
+        #查找当前的根节点在中序遍历中的位置
+        index = inorder.index(preorder[0]) 
+        # 划分左右子树
+        left_pre = preorder[1:index+1]
+        left_in = inorder[:index]
+        right_pre = preorder[index+1:]
+        right_in = inorder[index+1:]
+        # 遍历创建子树
+        node.left = self.buildTree(left_pre, left_in)
+        node.right = self.buildTree(right_pre, right_in)
+        # 返回当前节点
+        return node
---
+++ coding_reversion/124_maxpathsumbinarytree.py
@@ -0,0 +1,59 @@
+'''
+给定一个非空二叉树，返回其最大路径和。
+
+本题中，路径被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+
+ 
+
+示例 1：
+
+输入：[1,2,3]
+
+       1
+      / \
+     2   3
+
+输出：6
+示例 2：
+
+输入：[-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-maximum-path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        global res
+        res = float('-inf')
+        if not root:
+            return 0 
+        self.getmax(root)
+        return res 
+        
+
+
+    def getmax(self,root):
+        global res
+        if not root:
+            return 0 
+        left = max(0,self.getmax(root.left))
+        right = max(0,self.getmax(root.right))
+        res = max(res,root.val + left + right)
+        return max(left,right) + root.val
